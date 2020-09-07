dbfe045237b6cf4a445cbd00a0db1c6085d34488
Andoni del Olmo <andoni.delolmo@gmail.com>
2020-09-07 20:03:18 +0200

    style

--- main.go
+++ main.go
@@ -42,12 +42,11 @@ func configure() {
 	w := webview.New(true)
 	defer w.Destroy()
 	w.SetTitle("Delauncher")
-	w.SetSize(500, 160, webview.HintNone)
+	w.SetSize(500, 170, webview.HintNone)
 	w.Bind("save", func(serverUrl, password string) {
-		fmt.Printf("ServerUrl: %s  password: %s\n", serverUrl, password)
 		conf.Save(serverUrl, password)
 	})
-	w.Bind("quit", func() {
+	w.Bind("exit", func() {
 		w.Terminate()
 	})
 	type Config struct {
@@ -65,31 +64,62 @@ func configure() {
 			<head>
 				<style>
 					body {
-						background-color:#e8ebee;
+						background-color: #f5f7fa;
+						color: #656667;
+						font-family: Ubuntu, "times new roman", times, roman, serif;
+						font-size: 14px;
 					}
 					.container {
 						width: 480px;
 						clear: both;
 					}
 					.container input {
-						width: 280px;
+						color: #656667;
+						width: 350px;
 						clear: both;
 						float: right;
+						padding: 5px 5px;
+						text-decoration: none;
+						display: inline-block;
+						font-size: 14px;
+					}
+					.container input:focus  {
+						border: 1px solid orange;
+					}
+					form {
+						margin: 20px 0px 0px 0px;
 					}
 					fieldset {
-						background-color:#b0d7fa;
+						border: none;
 					}
 					label {
 						display: inline-block;
-						width: 160px;
-						text-align: right;
+						width: 80px;
+						padding: 10px 5px;
 						vertical-align: sub;
 					}
 					#buttons {
 						float: right;
 					}
-					button {
-						background-color:#bdcfdf;
+					button {						
+						color: #656667;
+						background-image: linear-gradient(#f5f7fa, #eef0f2);
+						border: 1px solid #b3bac0;
+						padding: 5px 28px;
+						text-align: center;
+						text-decoration: none;
+						display: inline-block;
+						font-size: 14px;
+						margin: 1px 1px;
+						border-radius: 5px;
+					}
+					button:hover {
+						border: 1px solid #5d5f62;	
+						margin: 1px 1px;
+					}
+					button:active {
+						background-image: none;
+						background-color: #e9ebee;
 					}
 				</style>
 			</head>
@@ -97,21 +127,15 @@ func configure() {
 				<div class="container">
 					<form>
 						<fieldset>
-							<legend>Configuration</legend>
-							<div class="block">
-								<label for="serverUrl">Deluge server URL:</label>
-								<input type="text" name="serverUrl" id="serverUrl">
-							</div>
-							<br/>
-							<div class="block">
-								<label for="password">Password:</label>
-								<input type="password" name="password" id="password">
-							</div>
-							<br/>
+							<legend>Deluge configuration</legend>
+							<label for="serverUrl">Server URL:</label>
+							<input type="text" name="serverUrl" id="serverUrl">
+							<label for="password">Password:</label>
+							<input type="password" name="password" id="password">
 						</fieldset>
 						<div id="buttons">
+							<button type="button" onclick="exit();">Exit</button>
 							<button type="button" onclick="save(document.getElementById('serverUrl').value, document.getElementById('password').value);">Save</button>
-							<button type="button" onclick="quit();">Quit</button>
 						</div>
 					</form>
 				</div>
